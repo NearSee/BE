a23f327793ed7750eddc3b9f68178875e99bcb48
woorimj <forestwr06@gmail.com>
2024-03-12 18:22:14 +0900

    feat: OAuth2UserService, SecurityConfig 파일 생성 카카오 로그인후 개인정보 동의

---
+++ src/main/java/com/Mung/NearSee/User/service/OAuth2UserService.java
@@ -0,0 +1,50 @@
+package com.Mung.NearSee.User.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class OAuth2UserService extends DefaultOAuth2UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(OAuth2UserService.class);
+
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        // Role generate
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_ADMIN");
+
+        // nameAttributeKey
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+
+        // DB 저장로직이 필요하면 추가
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        for(Map.Entry<String, Object> entry: attributes.entrySet()){
+            logger.info(entry.getKey());
+        }
+
+        return new DefaultOAuth2User(authorities, oAuth2User.getAttributes(), userNameAttributeName);
+    }
+}
+
+
+
+
+
