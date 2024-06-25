5e4c52853126522c3119aef80f86584ed362c746
woorimj <forestwr06@gmail.com>
2024-06-26 00:12:08 +0900

    Feat: 카카오유저도메인 설정

--- src/main/java/com/Mung/NearSee/User/Domain/KakaoUser.java
+++ src/main/java/com/Mung/NearSee/User/Domain/KakaoUser.java
@@ -1,12 +1,2 @@
 package com.Mung.NearSee.User.Domain;
 
-import jakarta.persistence.Entity;
-import lombok.Builder;
-import lombok.Getter;
-
-@Entity
-@Getter
-@Builder
-public class KakaoUser {
-
-}
