1aad2da2ee5952c42d080a310d05ad7ddfe6caf6
woorimj <forestwr06@gmail.com>
2024-08-25 01:23:28 +0900

    Init: project init

---
+++ src/main/java/Mung/NearSee/NearSeeApplication.java
@@ -0,0 +1,13 @@
+package Mung.NearSee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearSeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearSeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Mung/NearSee/NearSeeApplicationTests.java
@@ -0,0 +1,13 @@
+package Mung.NearSee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearSeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
